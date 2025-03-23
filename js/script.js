cc567ef225a2225c51f2fdecdd33256e7b8d31cc
Rakhman Karymshakov <karymshakovv@gmail.com>
2025-03-23 12:19:18 +0100

    Add searching logic.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 import { fetchAndProcessAllBankRates } from "./service/bankService.js";
 import { BankDisplay } from "./view/BankDisplay.js";
 import { SearchHandler } from "./view/SearchHandler.js";
+import { BankFilterService } from "./service/bankFilterService.js";
 
 const bankDisplay = new BankDisplay("bank-display");
-const searchHandler = new SearchHandler();
+const bankFilterService = new BankFilterService();
+const searchHandler = new SearchHandler(bankFilterService, bankDisplay);
 
 function displayBanksData(banks) {
   if (banks.length > 0) {
@@ -16,6 +18,7 @@ function displayBanksData(banks) {
 fetchAndProcessAllBankRates()
   .then((banks) => {
     console.log(`Successfully processed ${banks.length} banks`);
+    bankFilterService.setBanks(banks);
     displayBanksData(banks);
   })
   .catch((error) => {
--- js/view/searchHandler.js
+++ js/view/searchHandler.js
@@ -1,17 +1,33 @@
+import { BankFilterService } from "../service/bankFilterService.js";
+
 export class SearchHandler {
-  constructor() {
+  constructor(bankFilterService, bankDisplay) {
     this.bankSearchInput = document.getElementById("bank-search");
     this.currencyPairInput = document.getElementById("select-pair");
-
+    this.bankFilterService = bankFilterService;
+    this.bankDisplay = bankDisplay;
     this.setupEventListeners();
   }
 
   setupEventListeners() {
     // Bank search listener
     this.bankSearchInput.addEventListener("input", (event) => {
-      const searchTerm = event.target.value.toLowerCase();
-      console.log("Searching for bank:", searchTerm);
-      // TODO: Implement bank filtering
+      try {
+        const searchedBankName = event.target.value.toLowerCase();
+        console.log("Searching for bank:", searchedBankName);
+        const searchedBank =
+          this.bankFilterService.searchBank(searchedBankName);
+
+        if (!searchedBank || searchedBank.length === 0) {
+          console.log("No banks found matching search criteria");
+        }
+
+        this.bankDisplay.clearBanks();
+        searchedBank.forEach((bank) => this.bankDisplay.displayBank(bank));
+      } catch (error) {
+        console.error("Error in bank search:", error);
+        this.bankDisplay.clearBanks();
+      }
     });
 
     // Currency pair listener
