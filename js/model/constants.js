1696f71b8ff683f2c9cb41fbc1aa2484533b6eb6
Rakhman Karymshakov <karymshakovv@gmail.com>
2025-04-10 19:14:41 +0200

    Create a constants.js to store repeated code.

---
+++ js/model/constants.js
@@ -0,0 +1,18 @@
+export const PROVIDER_TYPE = {
+  BANK: "bank",
+  EXCHANGE: "exchange",
+  CRYPTO_EXCHANGE: "crypto-exchange",
+};
+
+export const FILTER_PROVIDER_TYPE = {
+  ALL: "all",
+  BANKS: "banks",
+  EXCHANGES: "exchanges",
+  CRYPTO_EXCHANGES: "crypto-exchanges",
+};
+
+export const SORT_OPTIONS = {
+  NO_SORT: "noRate",
+  BEST_BUY: "best-buy-rate",
+  BEST_SELL: "best-sell-rate",
+};
